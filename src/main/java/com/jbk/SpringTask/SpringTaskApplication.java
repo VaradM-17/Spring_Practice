bd305efcca3fb487af86f179d2575e12dd355bf0
VaradM-17 <varadmule17@gmail.com>
2024-11-29 17:02:12 +0530

    setup

---
+++ src/main/java/com/jbk/SpringTask/SpringTaskApplication.java
@@ -0,0 +1,14 @@
+package com.jbk.SpringTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTaskApplication.class, args);
+		System.out.println("Application Started...");
+	}
+
+}
