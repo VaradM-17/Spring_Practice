d5f0566da432f18fd9e30ef436e4d88564d8cfa6
VaradM-17 <varadmule17@gmail.com>
2024-11-29 17:23:29 +0530

    dao insert

--- src/main/java/com/jbk/SpringTask/controller/EmployeeController.java
+++ src/main/java/com/jbk/SpringTask/controller/EmployeeController.java
@@ -2,7 +2,6 @@ package com.jbk.SpringTask.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.persistence.Id;
 
 @RestController
 public class EmployeeController {
--- src/main/java/com/jbk/SpringTask/dao/EmployeeDao.java
+++ src/main/java/com/jbk/SpringTask/dao/EmployeeDao.java
@@ -1,8 +1,45 @@
 package com.jbk.SpringTask.dao;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.jbk.SpringTask.entity.Employee;
 
 @Repository
 public class EmployeeDao {
 
+	@Autowired
+	SessionFactory factory;
+
+	public String insertdata(Employee emp) {
+		Session ss = null;
+		Transaction tx = null;
+		String msg = null;
+
+		try {
+			ss = factory.openSession();
+			tx = ss.beginTransaction();
+
+			ss.persist(emp);
+
+			tx.commit();
+			
+			msg = "Data successfully inserted...";
+		} catch (Exception e) {
+
+			if (tx != null) {
+				tx.rollback();
+			}
+
+			e.printStackTrace();
+		} finally {
+			if (ss != null) {
+				ss.close();
+			}
+		}
+		return msg;
+	}
 }
