674be427dc14f871eff732a8a48149e09e4902e3
VaradM-17 <varadmule17@gmail.com>
2024-11-30 11:19:24 +0530

    controller delete added

--- src/main/java/com/jbk/SpringTask/controller/EmployeeController.java
+++ src/main/java/com/jbk/SpringTask/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.jbk.SpringTask.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -25,6 +26,12 @@ public class EmployeeController {
 	@PutMapping("/updatedata/{id}")
 	public String updateData(@RequestBody Employee emp, @PathVariable int id) {
 		String msg = employeeservice.updateData(emp, id);
+		return msg;
+	}
+	
+	@DeleteMapping("/deletedata/{id}")
+	public String deleteData(@PathVariable int id) {
+		String msg = employeeservice.deleteData(id);
 		return msg;
 	}
 }
