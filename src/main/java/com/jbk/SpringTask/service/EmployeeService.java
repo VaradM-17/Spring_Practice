f2128d6c0ec6f4e59d8a0c18e0468fd499df7bbb
VaradM-17 <varadmule17@gmail.com>
2024-11-30 11:09:47 +0530

    service insert added

--- src/main/java/com/jbk/SpringTask/service/EmployeeService.java
+++ src/main/java/com/jbk/SpringTask/service/EmployeeService.java
@@ -1,9 +1,26 @@
 package com.jbk.SpringTask.service;
 
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.jbk.SpringTask.dao.EmployeeDao;
+import com.jbk.SpringTask.entity.Employee;
 
 @Service
 public class EmployeeService {
+	@Autowired
+	EmployeeDao employeedao;
 
+	public String insertData(Employee emp) {
+
+		String msg = employeedao.insertdata(emp);
+
+		if (Objects.isNull(msg)) {
+			msg = "Data insertion failed,try again.";
+		}
+		return msg;
+	}
 	
 }
