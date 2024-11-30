faf5a4475183c6280c9432ede2de0c99991f63cf
VaradM-17 <varadmule17@gmail.com>
2024-11-30 11:17:52 +0530

    controller insert added

--- src/main/java/com/jbk/SpringTask/controller/EmployeeController.java
+++ src/main/java/com/jbk/SpringTask/controller/EmployeeController.java
@@ -1,10 +1,22 @@
 package com.jbk.SpringTask.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jbk.SpringTask.entity.Employee;
+import com.jbk.SpringTask.service.EmployeeService;
 
 @RestController
 public class EmployeeController {
-	
 
+	@Autowired
+	EmployeeService employeeservice;
+
+	@PostMapping("/insertdata")
+	public String insertData(@RequestBody Employee emp) {
+		String msg = employeeservice.insertData(emp);
+		return msg;
+	}
 }
