190a94f4d1bae56f37d361e880238b7bfdef9b1e
VaradM-17 <varadmule17@gmail.com>
2024-11-30 11:18:42 +0530

    controller update added

--- src/main/java/com/jbk/SpringTask/controller/EmployeeController.java
+++ src/main/java/com/jbk/SpringTask/controller/EmployeeController.java
@@ -1,7 +1,9 @@
 package com.jbk.SpringTask.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +19,12 @@ public class EmployeeController {
 	@PostMapping("/insertdata")
 	public String insertData(@RequestBody Employee emp) {
 		String msg = employeeservice.insertData(emp);
+		return msg;
+	}
+	
+	@PutMapping("/updatedata/{id}")
+	public String updateData(@RequestBody Employee emp, @PathVariable int id) {
+		String msg = employeeservice.updateData(emp, id);
 		return msg;
 	}
 }
